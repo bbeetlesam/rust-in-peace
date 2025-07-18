90b6311564b3b5da2c1c401f0c91f859fc0d0719
bbeetlesam <samsismines@gmail.com>
2025-07-19 04:24:24 +0700

    add: bird chirps backsound

--- src/sounds.lua
+++ src/sounds.lua
@@ -5,6 +5,7 @@ local path = "assets/sfx/"
 function sounds.load()
     sounds.clockTick = love.audio.newSource(path .. "clock-tick.ogg", "static")
     sounds.rollingSolid = love.audio.newSource(path .. "rolling-wheels-solid.ogg", "static")
+    sounds.morningChirps = love.audio.newSource(path .. "morning-chirps.ogg", "stream")
 end
 
 return sounds
